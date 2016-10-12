91a7de6533d140da514164e205d3db9f5473329a
JPBlancoDB <jpblancodb@gmail.com>
2016-10-11 21:54:10 -0300

    Added middleware for checking user type

---
+++ app/Http/Middleware/CheckType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckType
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (is_null($request->user())) {
+            return redirect('/');
+        }
+
+        if (!$request->user()->hasType($type)) {
+            return redirect('home');
+        }
+
+        return $next($request);
+    }
+}
