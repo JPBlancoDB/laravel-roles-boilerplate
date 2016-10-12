7ac90884cfa8884cbb6698f05618411ae1464010
JPBlancoDB <jpblancodb@gmail.com>
2016-10-11 22:19:22 -0300

    Added relation between roles and permissions

--- app/Permission.php
+++ app/Permission.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     protected $fillable = [
-        'name', 'description'
+        'name',
+        'description'
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
